fc692f8dba4995b49059df0417226ea14bba8b43
Summersite <jimmy_johannsen@yahoo.com>
2019-02-05 22:19:33 -0500

    Update Bypass.ps1
    added tesxt

--- Bypass.ps1
+++ Bypass.ps1
@@ -3,3 +3,4 @@
 powershell.exe -executionpolicy bypass -File D:\UPFW.ps1
 # Set-ExecutionPolicy -ExecutionPolicy undefined -Scope currentuser -Force
 # Set-ExecutionPolicy undefined -Scope Process -Force
+# this is a comment
