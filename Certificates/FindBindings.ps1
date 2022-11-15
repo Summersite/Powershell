a7c94882b741e1b3daf30c86370b8cd731867609
Summersite <jimmy_johannsen@yahoo.com>
2022-11-15 10:14:39 -0500

    Create FindBindings.ps1

---
+++ Certificates/FindBindings.ps1
@@ -0,0 +1,18 @@
+﻿Import-Module -Name WebAdministration
+
+Get-ChildItem -Path IIS:SSLBindings | ForEach-Object -Process `
+{
+    if ($_.Sites)
+    {
+        $certificate = Get-ChildItem -Path CERT:LocalMachine/My |
+            Where-Object -Property Thumbprint -EQ -Value $_.Thumbprint
+
+        [PsCustomObject]@{
+            Sites                        = $_.Sites.Value
+            CertificateFriendlyName      = $certificate.FriendlyName
+            CertificateDnsNameList       = $certificate.DnsNameList
+            CertificateNotAfter          = $certificate.NotAfter
+            CertificateIssuer            = $certificate.Issuer
+        }
+    }
+}
