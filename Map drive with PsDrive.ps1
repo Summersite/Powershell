342730037f2bec299e057713b5c863d37ee293c7
Summersite <jimmy_johannsen@yahoo.com>
2022-12-15 15:10:33 -0500

    PSDrive mapping

---
+++ Map drive with PsDrive.ps1
@@ -0,0 +1,3 @@
+﻿# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-psdrive?view=powershell-7.3
+# Create new Mapped Drive connection
+New-PSDrive -Persist -Name "X" -PSProvider "FileSystem" -Root "\\vhost01\f$" -Scope Global
